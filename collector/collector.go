bce266d5f8b665a149883cdd2636644452634863
Steven Kreuzer <skreuzer@FreeBSD.org>
2018-04-04 09:35:50 -0400

    go fmt

--- collector/collector.go
+++ collector/collector.go
@@ -1,61 +1,61 @@
 package collector
 
 import (
-        "github.com/aws/aws-sdk-go/aws"
-        "github.com/aws/aws-sdk-go/aws/session"
-        "log"
-        "github.com/aws/aws-sdk-go/service/sqs"
-        "net/http"
-        "strings"
-        "fmt"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+	"log"
+	"net/http"
+	"strings"
 )
 
 type MetricHandler struct{}
 
 func (h MetricHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-        queues := getQueues()
-        for queue, attr := range queues {
-                msgAvailable := *attr.Attributes["ApproximateNumberOfMessages"]
-                msgDelayed := *attr.Attributes["ApproximateNumberOfMessagesDelayed"]
-                msgNotvisible := *attr.Attributes["ApproximateNumberOfMessagesDelayed"]
-                fmt.Fprintf(w, "sqs_messages_visible{queue_name=\"%s\"} %+v\n", queue, msgAvailable)
-                fmt.Fprintf(w, "sqs_messages_delayed{queue_name=\"%s\"} %+v\n", queue, msgDelayed)
-                fmt.Fprintf(w, "sqs_messages_not_visible{queue_name=\"%s\"} %+v\n", queue, msgNotvisible)
-        }
+	queues := getQueues()
+	for queue, attr := range queues {
+		msgAvailable := *attr.Attributes["ApproximateNumberOfMessages"]
+		msgDelayed := *attr.Attributes["ApproximateNumberOfMessagesDelayed"]
+		msgNotvisible := *attr.Attributes["ApproximateNumberOfMessagesDelayed"]
+		fmt.Fprintf(w, "sqs_messages_visible{queue_name=\"%s\"} %+v\n", queue, msgAvailable)
+		fmt.Fprintf(w, "sqs_messages_delayed{queue_name=\"%s\"} %+v\n", queue, msgDelayed)
+		fmt.Fprintf(w, "sqs_messages_not_visible{queue_name=\"%s\"} %+v\n", queue, msgNotvisible)
+	}
 }
 
 func getQueueName(url string) (queueName string) {
-        queue := strings.Split(url, "/")
-        queueName = queue[len(queue) -1 ]
-        return
+	queue := strings.Split(url, "/")
+	queueName = queue[len(queue)-1]
+	return
 }
 
 func getQueues() (queues map[string]*sqs.GetQueueAttributesOutput) {
-        sess := session.Must(session.NewSession())
-        client := sqs.New(sess)
-        result, err := client.ListQueues(nil)
-        if err != nil {
-                log.Fatal("Error ", err)
-        }
+	sess := session.Must(session.NewSession())
+	client := sqs.New(sess)
+	result, err := client.ListQueues(nil)
+	if err != nil {
+		log.Fatal("Error ", err)
+	}
 
-        queues = make(map[string]*sqs.GetQueueAttributesOutput)
+	queues = make(map[string]*sqs.GetQueueAttributesOutput)
 
-        if result.QueueUrls == nil {
-                log.Println("Couldnt find any queues in region:", *sess.Config.Region)
-        }
-        for _, urls := range result.QueueUrls {
-                params := &sqs.GetQueueAttributesInput{
-                        QueueUrl: aws.String(*urls),
-                        AttributeNames: []*string{
-                                aws.String("ApproximateNumberOfMessages"),
-                                aws.String("ApproximateNumberOfMessagesDelayed"),
-                                aws.String("ApproximateNumberOfMessagesNotVisible"),
-                        },
-                }
+	if result.QueueUrls == nil {
+		log.Println("Couldnt find any queues in region:", *sess.Config.Region)
+	}
+	for _, urls := range result.QueueUrls {
+		params := &sqs.GetQueueAttributesInput{
+			QueueUrl: aws.String(*urls),
+			AttributeNames: []*string{
+				aws.String("ApproximateNumberOfMessages"),
+				aws.String("ApproximateNumberOfMessagesDelayed"),
+				aws.String("ApproximateNumberOfMessagesNotVisible"),
+			},
+		}
 
-                resp, _ := client.GetQueueAttributes(params)
-                queueName := getQueueName(*urls)
-                queues[queueName] = resp
-        }
-        return queues
+		resp, _ := client.GetQueueAttributes(params)
+		queueName := getQueueName(*urls)
+		queues[queueName] = resp
+	}
+	return queues
 }
--- main.go
+++ main.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-        "flag"
-        "net/http"
-        "log"
+	"flag"
+	"log"
+	"net/http"
 
-        "github.com/ashiddo11/sqs-exporter/collector"
+	"github.com/ashiddo11/sqs-exporter/collector"
 )
 
 var addr = flag.String("", ":9434", "The address to listen on for HTTP requests.")
 
 func logRequest(handler http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
-        handler.ServeHTTP(w, r)
-    })
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
 }
 
 func main() {
-        flag.Parse()
-        http.Handle("/metrics", collector.MetricHandler{})
-        log.Println("Starting exporter on", *addr)
-        log.Fatal(http.ListenAndServe(*addr, logRequest(http.DefaultServeMux)))
+	flag.Parse()
+	http.Handle("/metrics", collector.MetricHandler{})
+	log.Println("Starting exporter on", *addr)
+	log.Fatal(http.ListenAndServe(*addr, logRequest(http.DefaultServeMux)))
 }
