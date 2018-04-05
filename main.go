6e9070e4ff4b10cd42775e0db74a72350e1d1f1d
Steven Kreuzer <skreuzer@FreeBSD.org>
2018-04-05 15:45:46 -0400

    Standardize flags
    Allow for the listening address to be specified with -web.listen-address
    Allow for the metrics endpoint to be specified with -web.telemetry-path

--- main.go
+++ main.go
@@ -8,8 +8,6 @@ import (
 	"github.com/ashiddo11/sqs-exporter/collector"
 )
 
-var addr = flag.String("", ":9434", "The address to listen on for HTTP requests.")
-
 func logRequest(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
@@ -18,17 +16,23 @@ func logRequest(handler http.Handler) http.Handler {
 }
 
 func main() {
+	var (
+		listenAddress = flag.String("web.listen-address", ":9434", "Address to listen on for web interface and telemetry.")
+		metricsPath   = flag.String("web.telemetry-path", "/metrics", "Path under which to expose metrics.")
+	)
 	flag.Parse()
-	http.Handle("/metrics", collector.MetricHandler{})
+
+	http.Handle(*metricsPath, collector.MetricHandler{})
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(`
 	        <html>
 	        <head><title>SQS Exporter</title></head>
 	        <body>
-	        <h1>SQS Exporter</h1><p><a href="/metrics">Metrics</a></p>
+	        <h1>SQS Exporter</h1>
+	        <p><a href='` + *metricsPath + `'>Metrics</a></p>
 	        </body>
 	        </html>`))
 	})
-	log.Println("Starting exporter on", *addr)
-	log.Fatal(http.ListenAndServe(*addr, logRequest(http.DefaultServeMux)))
+	log.Println("Starting exporter on", *listenAddress)
+	log.Fatal(http.ListenAndServe(*listenAddress, logRequest(http.DefaultServeMux)))
 }
